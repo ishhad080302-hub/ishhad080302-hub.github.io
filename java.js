782c577773ad8ffc3f722c621e3a75129fd675e1
isak128 <ishhad080302@student.jenseneducation.se>
2025-12-16 11:40:48 +0100

    hej

---
+++ java.js
@@ -0,0 +1,18 @@
+let header = document.getElementsByTagName("Header");
+ 
+ header[0].innerHTML = ` <nav class="navbar">
+        <div class="logo">
+            <img src="Lcloga-min.png" alt="LC Meals Logo" class="logo-img">
+        </div>
+
+        <ul class="nav-links">
+            <li><a href="Home.html">Home</a></li>
+            <li><a href="food.html">Food</a></li>
+            <li><a href="beställ.html">Beställ nu</a></li>
+        </ul>
+    </nav>
+`
+let footer = document.getElementsByTagName("Footer");
+
+footer[0].innerHTML = `<h3></h3>
+<p>Welcome to lc with the best Meals!  © 2025</p>`
